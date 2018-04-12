4e78e47e0259ebf93757e1ff87c5718f418ff663
Luis David De la cruz <delacruz_luis@outlook.es>
2018-04-12 11:02:54 -0500

    SELECT * FROM T_Registro TO LIST ---------------------- Se seleccionan los registros y se ponen en un ListView

--- SQLiteDemo/SQLiteDemo/SQLiteDemo/Vistas/UI_ConsultaRegistro.xaml.cs
+++ SQLiteDemo/SQLiteDemo/SQLiteDemo/Vistas/UI_ConsultaRegistro.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,19 @@ namespace SQLiteDemo
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class UI_ConsultaRegistro : ContentPage
 	{
-		public UI_ConsultaRegistro ()
+        private SQLiteAsyncConnection _conn;
+        private ObservableCollection<T_Registro> _TablaRegistro;
+        public UI_ConsultaRegistro ()
 		{
 			InitializeComponent ();
-		}
-	}
+            _conn = DependencyService.Get<ISQLiteDB>().GetConnection();
+        }
+        protected async override void OnAppearing()
+        {
+            var ResulRegistros = await _conn.Table<T_Registro>().ToListAsync();
+            _TablaRegistro = new ObservableCollection<T_Registro>(ResulRegistros);
+            ListaUsuarios.ItemsSource = _TablaRegistro;
+            base.OnAppearing();
+        }
+    }
 }
